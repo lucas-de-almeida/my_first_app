2b0944fb7d8f73ada06a385e1d96503a7f944fbb
Lucas de Almeida <lucasde.almeida@hotmail.comgit config --global user.email lucasde.almeida@hotmail.comgit config --global user.email lucasde.almeida@hotmail.com>
2020-06-29 00:11:51 -0300

    adicionado random color metodos _clickLong/short

--- lib/my_first_app.dart
+++ lib/my_first_app.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:random_color/random_color.dart';
 
 class FirstApp extends StatefulWidget {
   @override
   _FirstAppState createState() => _FirstAppState();
 }
 
+RandomColor _randomColor = RandomColor();
+
+Color _color = _randomColor.randomColor();
+
 class _FirstAppState extends State<FirstApp> {
   int _calc = 1;
+
   void _clicklLong() {
     print('cliclou long');
+    _color = _randomColor.randomColor();
     setState(() {
       _calc = 1;
     });
@@ -16,7 +23,7 @@ class _FirstAppState extends State<FirstApp> {
 
   void _clickShort() {
     print('cliclou short');
-
+    _color = _randomColor.randomColor();
     setState(() {
       _calc = _calc * 2;
     });
@@ -32,7 +39,7 @@ class _FirstAppState extends State<FirstApp> {
         child: Center(
           child: Text(
             'Calc: $_calc',
-            style: TextStyle(fontSize: 28),
+            style: TextStyle(color: _color, fontSize: 28),
           ),
         ),
         onTap: _clickShort,
